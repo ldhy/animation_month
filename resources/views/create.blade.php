e4fd0c5bc51f10fb42b930bcd6f07acd245b20e6
ldhy <laura.delahaye@gmail.com>
2017-12-22 12:01:21 +0100

    Ajout calendar dans view create

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,81 +5,64 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Calendar</title>
+        <h1>Calendar</h1>
+          <div class="box">
+              <div class="item">1</div>
+              <div class="item">2</div>
+              <div class="item">3</div>
+              <div class="item">4</div>
+              <div class="item">5</div>
+              <div class="item">6</div>
+              <div class="item">7</div>
+              <div class="item">8</div>
+              <div class="item">9</div>
+              <div class="item">10</div>
+              <div class="item">11</div>
+              <div class="item">12</div>
+              <div class="item">13</div>
+              <div class="item">14</div>
+              <div class="item">15</div>
+              <div class="item">16</div>
+              <div class="item">17</div>
+              <div class="item">18</div>
+              <div class="item">19</div>
+              <div class="item">20</div>
+              <div class="item">21</div>
+              <div class="item">22</div>
+              <div class="item">23</div>
+              <div class="item">24</div>
+              <div class="item">25</div>
+              <div class="item">26</div>
+              <div class="item">27</div>
+              <div class="item">28</div>
+              <div class="item">29</div>
+              <div class="item">30</div>
+              <div class="item">31</div>
+          </div>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
+            .item {
+            font-family: impact;
+            color:#1d1d1d;
+            background-color: #ccc;
+            text-align: center;
+            font-size: 2rem;
+            padding: 0.5rem 0;
+            width: calc(10% - 0.5rem);
+            min-width: 200px;
+            margin-bottom: 0.5rem;
+
             }
 
-            .full-height {
-                height: 100vh;
-            }
+            .box{
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around;
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
             }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <p>Create</p>
-            </div>
-        </div>
-    </body>
 </html>
